d128fdcb73b681614f466758293d55289532a125
Khushichauhan <kc6478576@gmail.com>
2024-07-19 14:01:31 +0530

    Day6 Completed

--- Day6/01-arrayCreation&Acces.js
+++ Day6/01-arrayCreation&Acces.js
@@ -2,10 +2,12 @@
 
 // Activity 1: Array Creation and Access
 // Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+
 let numbers = [1, 2, 3, 4, 5];
 console.log("Task 1:", numbers);
 
 // Task 2: Access the first and last elements of the array and log them to the console.
+
 let firstElement = numbers[0];
 let lastElement = numbers[numbers.length - 1];
 console.log("Task 2: First Element:", firstElement, "Last Element:", lastElement);
